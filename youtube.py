515cfa89773134552a1db1413b87ce22fbac96cc
trentwiles <wilest7@gmail.com>
2023-10-23 13:45:35 -0700

    yt api

---
+++ youtube.py
@@ -0,0 +1,20 @@
+import requests
+
+def getChannelVideos(apiKey, id):
+    r = requests.get(f"https://www.googleapis.com/youtube/v3/search?key={apiKey}&channelId={id}&part=id&maxResults=10")
+    
+    videos = []
+
+    for x in r.json()["items"]:
+        if x["id"]["kind"] == "youtube#video":
+            videos.append(x["id"]["videoId"])
+    return videos
+
+def getVideoMetadata(apiKey, id):
+    r = requests.get(f"https://www.googleapis.com/youtube/v3/videos?key={apiKey}&id={id}&part=snippet")
+    
+    title = r.json()["items"][0]["snippet"]["title"]
+    desc = r.json()["items"][0]["snippet"]["description"]
+    thumb = r.json()["items"][0]["snippet"]["thumbnails"]["medium"]["url"]
+
+    return {"title": title, "desc": desc, "thumbnail": thumb}
