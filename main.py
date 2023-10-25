f6806a9e8d5e5a37863090e6e93d620a010d4dd7
trentwiles <wilest7@gmail.com>
2023-10-24 20:49:51 -0700

    spotify works!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import youtube
 import json
+import spotify
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def index():
     apiKey = json.loads(open("config.json").read())["youtube"]
     channel = json.loads(open("config.json").read())["channel"]
-    return render_template('index.html', videos = youtube.getChannelVideosAndData(apiKey, channel))
+    return render_template('index.html', videos = youtube.getChannelVideosAndData(apiKey, channel), podcasts=spotify.getPodcast())
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ spotify.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def getToken():
+    api = json.loads(open("config.json").read())
+    clientID = api["spotify_client_id"]
+    clientSecret = api["spotify_secret"]
+    r = requests.post("https://accounts.spotify.com/api/token", data={"grant_type": "client_credentials", "client_id": clientID, "client_secret": clientSecret})
+    return r.json()["access_token"]
+
+def getPodcast():
+    api = json.loads(open("config.json").read())
+
+    podcastID = api["podcastID"]
+    token = getToken()
+
+    r = requests.get(f"https://api.spotify.com/v1/shows/{podcastID}/episodes", headers={"Authorization": f"Bearer {token}"})
+
+    podcasts = []
+    for x in r.json()["items"]:
+        podcasts.append({"preview": x["audio_preview_url"], "link": x["external_urls"]["spotify"], "description": x["description"], "time": str(x['duration_ms']/1000) + "s", "title": x["name"], "image": x["images"][0]["url"]})
+    return podcasts
