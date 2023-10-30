98a2b9fab68a27d456ea99cf240d29ae0cfa0164
trentwiles <wilest7@gmail.com>
2023-10-30 13:58:12 -0700

    almost accidently uploaded file

--- main.py
+++ main.py
@@ -15,27 +15,31 @@ def index():
 
 @app.route('/upload')
 def upload():
-    if session['isLoggedIn'] != True:
-        return render_template("login.html")
-    return render_template('upload.html')
+    try:
+        if session['isLoggedIn'] != True:
+            return render_template("login.html")
+        return render_template('upload.html')
+    except:
+        return render_template('upload.html')
 
-@app.route('/doUpload')
+@app.route('/doUpload', methods=["POST"])
 def uploadFile():
     if 'file' not in request.files:
-        return 'No file part'
+        return render_template("msg.html", msg="Missing a file")
     
     file = request.files['file']
     
     if file.filename == '':
-        return 'No selected file'
+        return render_template("msg.html", msg="Select a file!")
     
-    if file.filename.split('.')[len(file.filename.split('.')) - 1] not in ["mp3", "wav", "aac", "ogg"]:
-        return 'Bad file ext'
+    valid = ["mp3", "wav", "aac", "ogg"]
+    if file.filename.split('.')[len(file.filename.split('.')) - 1] not in valid:
+        return render_template("msg.html", msg=f"Bad File Extension (we accept {', '.join(valid)})")
 
     if file:
-        filename = str(random.randint(0,100000000)) + file.filename
+        filename = 'storage/' + str(random.randint(0,100000000)) + file.filename.replace(" ", "-")
         file.save(filename)
-        return 'File successfully uploaded! ' + filename
+        return render_template("msg.html", msg=f"Uploaded podcast file {filename}")
 
 
 if __name__ == '__main__':
